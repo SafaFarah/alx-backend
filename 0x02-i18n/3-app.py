9653df9a1033c034f27d83a5ff15bc2abf6cc4a4
SafaFarah <kangjisook13@gmail.com>
2024-09-22 15:55:47 +0300

    update templates/3-index.html

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -3,32 +3,41 @@
 Flask app with Babel for i18n and locale selection
 """
 
-
-from flask import Flask, render_template
-from flask_babel import Babel, _
-
-app = Flask(__name__)
-babel = Babel(app)
+from flask import Flask, render_template, request
+from flask_babel import Babel
 
 
 class Config:
-    LANGUAGES = ['en', 'fr']
+    """
+    Configuration class for Flask-Babel.
+    """
+
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+
+babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
+    """
+    Select the best match for supported languages.
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
+    """
+    Render the index page.
+    """
     return render_template('3-index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
